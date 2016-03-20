90d8cb3aa5a71eadcfff362f2e04fd2ffd512a85
bu <bulat87@gmail.com>
2016-03-20 22:41:04 +0300

    Add simple form for new and add some gems

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,5 +1,44 @@
 class WorkoutsController < ApplicationController
-	def index
-		
-	end
+  def index
+    
+  end
+
+  def show
+    
+  end
+
+  def new
+    @workout = Workout.new
+  end
+
+  def create
+    @workout = Workout.new(workouts_params)
+    if @workout.save
+      redirect_to @workout
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+  def workouts_params
+    params.require(:workout).permit(:date, :workout, :rating, :length)
+  end
+
+  def find_workout
+    
+  end
 end
