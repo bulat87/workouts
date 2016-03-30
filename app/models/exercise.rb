7f2259523d38963e961f951feb0aaa70b6c071a4
bu <bulat87@gmail.com>
2016-03-30 23:02:43 +0300

    Add validations to models and some fixes for views

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,8 @@
 class Exercise < ActiveRecord::Base
   belongs_to :workout
+  validates :name, :workout_type, :link, :description, presence: true
+  validates :name, length: { maximum: 100 }
+  validates :workout_type, length: { maximum: 100 }
+  validates :link, length: { maximum: 200 }
+  validates :description, length: { maximum: 1000 }
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,4 +1,6 @@
 class Workout < ActiveRecord::Base
   has_many :exercises, dependent: :destroy
   belongs_to :user
+  validates :date, :workout, :length, presence: true
+  validates :date, :workout, :rating, :length, length: { maximum: 200 }
 end
